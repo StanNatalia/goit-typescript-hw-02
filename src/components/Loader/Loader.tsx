58bf7c99485940beacadd07cc0b4eb218bf5ab34
Natalia Stan <popkids.ua@gmail.com>
2025-03-12 18:10:52 +0200

    fix radius

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,17 +2,24 @@ import { Audio } from "react-loader-spinner";
 
 interface LoaderProps {
   isLoading: boolean;
+  height?: string;
+  width?: string;
+  color?: string;
 }
 
-const Loader: React.FC<LoaderProps> = ({ isLoading }) => {
+const Loader: React.FC<LoaderProps> = ({
+  isLoading,
+  height = "80",
+  width = "80",
+  color = "red,",
+}) => {
   return (
     <div>
       {isLoading && (
         <Audio
-          height="80"
-          width="80"
-          radius={9}
-          color="green"
+          height={height}
+          width={width}
+          color={color}
           ariaLabel="three-dots-loading"
         />
       )}
