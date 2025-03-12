0258dc7abc2433c9d2f5516eb6b75fafbcb38e59
Natalia Stan <popkids.ua@gmail.com>
2025-03-12 10:00:21 +0200

    HW2

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.js";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <>
-      <App />
-      <Toaster position="top-center" />
-    </>
-  </StrictMode>
-);
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <>
+        <App />
+        <Toaster position="top-center" />
+      </>
+    </StrictMode>
+  );
+}
